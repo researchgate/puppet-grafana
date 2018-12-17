2cb212b766f783b83a27ccf3cf903be056bb6fa8
Ernad Halilovic <ernad.halilovic@researchgate.net>
2018-12-17 10:30:31 +0100

    Add a forward slash for plugin url

--- lib/puppet/provider/grafana_plugin/grafana_cli.rb
+++ lib/puppet/provider/grafana_plugin/grafana_cli.rb
@@ -46,7 +46,7 @@ Puppet::Type.type(:grafana_plugin).provide(:grafana_cli) do
 
   def create
     if resource[:repo]
-      plugin_url = "--pluginUrl #{resource[:repo]}#{resource[:name]}-#{resource[:version]}.#{resource[:extension]}"
+      plugin_url = "--pluginUrl #{resource[:repo]}/#{resource[:name]}-#{resource[:version]}.#{resource[:extension]}"
       grafana_cli(plugin_url, 'plugins', 'install', resource[:name])
     else
       grafana_cli('plugins', 'install', resource[:name])
